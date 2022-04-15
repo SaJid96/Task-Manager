2ac9d42a20137c288eaaae23d034c035c539a4a0
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-15 23:05:35 +0530

    Data Validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator=require('validator')
+
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -7,38 +9,58 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a postive number');
+      }
+    },
   },
 });
 
-// const me = new User({
-//   name: 'Sajid',
-//   age: 57,
-// });
+const me = new User({
+  name: 'colkcke',
+  email:'kc',
+  
+});
 
-// me.save()
-//   .then(() => console.log(me))
-//   .catch((e) => {
-//     console.log('Error!', e);
-//   });
-
-
-const Tasks=mongoose.model('Tasks',{
-    description:{
-        type:String,
-    },
-    completed:{
-        type:Boolean,
-    },
-})
-
-const task=new Tasks({
-    description:'ReactJS',
-    completed:true,
-})
-
-task.save().then(()=>console.log(task)).catch((e) => {
+me.save()
+  .then(() => console.log(me))
+  .catch((e) => {
     console.log('Error!', e);
   });
+
+
+// const Tasks=mongoose.model('Tasks',{
+//     description:{
+//         type:String,
+//     },
+//     completed:{
+//         type:Boolean,
+//     },
+// })
+
+// const task=new Tasks({
+//     description:'ReactJS',
+//     completed:true,
+// })
+
+// task.save().then(()=>console.log(task)).catch((e) => {
+//     console.log('Error!', e);
+//   });
