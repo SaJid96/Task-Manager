ba26884b05e3b66b22f759c07b85b2528cc17aa9
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-17 23:43:19 +0530

    REST API for creating task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,21 +6,3 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
 });
 
 
-
-
-const Tasks=mongoose.model('Tasks',{
-    description:{
-        type:String,
-        trim:true,
-        required:true,
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    },
-})
-
-const task=new Tasks({
-    description:'ReactJS',
-    
-})
--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
-const express=require('express')
-require('./db/mongoose')
+const express = require('express');
+require('./db/mongoose');
 
-const User=require('./models/user')
+const User = require('./models/user');
+const Task = require('./models/task');
 
-const app=express()
+const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
 
-app.post('/users',(req,res)=>{
-
-
-    const user= new User(req.body)
-
-    user.save().
-    then(
-        ()=>{
-res.send(user)
-        }
-    ).catch((e)=>{
-        res.status(400).send(e)
+  user
+    .save()
+    .then(() => {
+      res.send(user);
     })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
-})
-const port=process.env.PORT || 3000
 
-app.listen(port,()=>{
-    console.log(`Server is up on Port ${port}`);
-})
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is up on Port ${port}`);
+});
