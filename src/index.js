f3f1024d8d5fee3fe2e0eb5bb0a60fd28d93abc2
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-17 23:09:12 +0530

    user end-point created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express=require('express')
 
 const app=express()
 
+
+app.use(express.json())
+
+app.post('/users',(req,res)=>{
+console.log(req.body);
+    res.send('testing!!')
+})
 const port=process.env.PORT || 3000
 
 app.listen(port,()=>{
