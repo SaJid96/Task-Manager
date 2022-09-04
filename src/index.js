8098e6e7b7d28ba4beb03d9d2b876bbcf1fed6ca
SaJid96 <sajidhussain98@gmail.com>
2022-09-04 06:58:25 +0530

    express middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 8000
 
+
+app.use((req, res, next) => {
+
+
+    res.status(503).send('website under maintance,Check back soon!!!')
+
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -17,13 +25,13 @@ app.listen(port, () => {
 const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-   
-    const token= jwt.sign({_id:'abc123'},'thisismytest',{expiresIn:'7 days'})
 
-    console.log("Token is === "+token);
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismytest', { expiresIn: '7 days' })
 
-    const data=jwt.verify(token,'thisismytest')
-console.log('Data is === '+ data);
+    console.log("Token is === " + token);
+
+    const data = jwt.verify(token, 'thisismytest')
+    console.log('Data is === ' + data);
 
 }
 
