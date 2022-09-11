cae727751dbfe59a2949746c3deb9a3c9b50db7f
SaJid96 <sajidhussain98@gmail.com>
2022-09-11 11:09:12 +0530

    user task relatonship

--- src/index.js
+++ src/index.js
@@ -26,3 +26,21 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+
+const Task=require('./models/task')
+const User=require('./models/user')
+
+const main=async ()=>{
+    // const task= await Task.findById('631d5f280d5e512bbccaa5b8')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner);
+
+    const user=await User.findById('631d5ea80d5e512bbccaa5b6');
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks);
+
+    
+}
+
+main()
+ 
