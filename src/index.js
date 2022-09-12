6c8f4c7dea30daea926560104ba8a915a2e706eb
SaJid96 <sajidhussain98@gmail.com>
2022-09-12 18:18:37 +0530

    pagination

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
