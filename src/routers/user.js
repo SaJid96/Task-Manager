e444b0297279e39693782fa7867e801412962fb2
SaJid96 <sajidhussain98@gmail.com>
2022-09-18 18:03:59 +0530

    validation changes

--- src/routers/user.js
+++ src/routers/user.js
@@ -98,11 +98,23 @@ router.delete('/users/me', auth ,async (req, res) => {
 const multer=require('multer')
 
 const upload=multer({
-    dest:'src/avatars'
+    dest:'src/avatars',
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please uplaod an image'))
+        }
+        cb(undefined,true)
+    }
 })
 
 router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
     res.send("uploaded sucessfully")
+},(error,req,res,next)=>{
+
+    res.status(400).send({error:error.message})
 })
 
 module.exports = router
