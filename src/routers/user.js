3b6452db69090b4d0c619c771d17ef1507a8887f
SaJid96 <sajidhussain98@gmail.com>
2022-09-18 20:16:24 +0530

    serving up files

--- src/routers/user.js
+++ src/routers/user.js
@@ -98,7 +98,7 @@ router.delete('/users/me', auth ,async (req, res) => {
 const multer=require('multer')
 
 const upload=multer({
-    dest:'src/avatars',
+    
     limits:{
         fileSize:1000000
     },
@@ -110,11 +110,40 @@ const upload=multer({
     }
 })
 
-router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
+router.post('/users/me/avatar',auth,upload.single('avatar'), async(req,res)=>{
+
+    req.user.avatar=req.file.buffer
+
+    await req.user.save()
+
     res.send("uploaded sucessfully")
 },(error,req,res,next)=>{
 
     res.status(400).send({error:error.message})
+})
+
+router.delete('/users/me/avatar',auth,async(req,res)=>{
+    req.user.avatar=undefined
+    await req.user.save()
+
+    res.send('Deleted succesfully')
+})
+
+
+router.get('/users/:id/avatar',async(req,res)=>{
+    try {
+        
+        const user=await User.findById(req.params.id)
+        if(!user || !user.avatar){
+
+            throw new Error()
+        }
+
+        res.set('Content-Type','image/jpg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 module.exports = router
